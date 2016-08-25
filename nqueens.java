562a425625ddb26b4ff53eb1dbb958a4158adcd0
Ubuntu <ubuntu@ip-172-31-37-195.us-west-2.compute.internal>
2016-08-25 07:29:34 +0000

    Initial working copy

---
+++ nqueens.java
@@ -0,0 +1,72 @@
+class nqueens {
+	int[] sol;
+	int used;
+
+	boolean works(int x, int y) {
+		for (int i = 0; i < used; i++) {
+			if ((sol[i] == y) || (Math.abs(x - i) == Math.abs(y - sol[i]))) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	boolean _solutionSearch(int x, int n) {
+		if (x == n) {
+			return true;
+		}
+
+		for (int y = 0; y < n; y++) {
+			if (works(x,y)) {
+				sol[x] = y;
+				used++;
+
+				if (_solutionSearch(x + 1, n)) {
+					return true;
+				}
+
+				used--;
+			}
+		}
+
+		return false;
+	}
+
+	boolean hasSolution(int n) {
+		sol = new int[n];
+		used = 0;
+		for (int i = 0; i < sol.length; i++) {
+			sol[i] = -1;
+		}
+
+		return _solutionSearch(0, n);
+	}
+
+	void printSolution() {
+		for (int i = 0; i < sol.length; i++) {
+			for (int j = 0; j < sol.length; j++) {
+				if (sol[j] != i) {
+					System.out.print("x");
+				} else {
+					System.out.print("q");
+				}
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		nqueens nq = new nqueens();
+
+		System.out.println("args len: " + args.length);
+
+		int len = Integer.parseInt(args[0]);
+
+		for (int i = 0; i < len; i++) {
+			System.out.println("Found solution? " + nq.hasSolution(i));
+			nq.printSolution();
+		}
+	}
+}
